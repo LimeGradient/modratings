1311bfecaf4b97c644e9ce485ca181f106851fa8
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-07-31 10:34:46 -0400

    work on popup

--- src/main.cpp
+++ src/main.cpp
@@ -5,58 +5,67 @@
 
 using namespace geode::prelude;
 
-enum Ratings {
-		EPIC,
-		MYTHIC,
-		LEGENDARY
-};
-
 class ModRating {
 public:
-	ModRating(std::string id, Ratings rate) {
+	std::string modID;
+	int rating;
+
+	ModRating(std::string id, int rate) {
 		this->modID = id;
 		this->rating = rate;
 	}
-
-protected:
-	std::string modID;
-	Ratings rating;
+	static std::string getRatingFromEnum(int rating) {
+		switch (rating) {
+			case 1:
+				return "GJ_epicCoin1_001.png";
+			case 2:
+				return "GJ_epicCoin2_001.png";
+			case 3:
+				return "GJ_epicCoin3_001.png";
+		}
+	}
 };
 
 EventListener<web::WebTask> m_webListener;
-std::vector<ModRating> mods;
+std::vector<ModRating*> modsList;
 
 $execute {
-	new EventListener<EventFilter<ModLogoUIEvent>>(+[](ModLogoUIEvent* event) {
-		m_webListener.bind([] (web::WebTask::Event* e) {
-			if (web::WebResponse* res = e->getValue()) {
-                if (res->ok()) {
-					auto json = res->json().unwrap();
-					auto mods = json["mods"].as_array();
-					for (auto mod : mods) {
-						
-					}
+	m_webListener.bind([] (web::WebTask::Event* e) {
+		if (web::WebResponse* res = e->getValue()) {
+			if (res->ok()) {
+				auto json = res->json().unwrap();
+				auto mods = json["mods"].as_array();
+				for (auto mod : mods) {
+					ModRating* modRating = new ModRating(mod["id"].as_string(), mod["rating"].as_int());
+					log::info("found mod. id: {}, rating: {}", modRating->modID, mod["rating"].as_int());
+					modsList.push_back(modRating);
 				}
-            } else if (e->isCancelled()) {
-                log::info("The request was cancelled... So sad :(");
-            }
-		});
+			}
+		} else if (e->isCancelled()) {
+			log::info("The request was cancelled... So sad :(");
+		}
+	});
 
-		auto req = web::WebRequest();
-		m_webListener.setFilter(req.get("https://raw.githubusercontent.com/LimeGradient/modratings/master/ratings.json"));
+	auto req = web::WebRequest();
+	m_webListener.setFilter(req.get("https://raw.githubusercontent.com/LimeGradient/modratings/master/ratings.json"));
 
-        if (event->getModID() == "geode.loader") {
-            // Remember: no assumptions, even trivial ones!
-            if (auto rating = CCSprite::createWithSpriteFrameName("GJ_epicCoin3_001.png")) {
-                rating->setScaleX(1.75f);
-                rating->setScaleY(1.75f);
-				rating->setAnchorPoint({0.5f, 0.6f});
-				rating->setZOrder(event->getSprite()->getZOrder() - 1);
-                // `event->getSprite()` is guaranteed to not be `nullptr` though
-                event->getSprite()->addChildAtPosition(rating, Anchor::Center);
-            }
-        }
+	new EventListener<EventFilter<ModLogoUIEvent>>(+[](ModLogoUIEvent* event) {
+		for (auto mod : modsList) {
+			if (event->getModID() == mod->modID) {
+				if (auto rating = CCSprite::createWithSpriteFrameName(ModRating::getRatingFromEnum(mod->rating).c_str())) {
+					rating->setScaleX(1.75f);
+					rating->setScaleY(1.75f);
+					rating->setAnchorPoint({0.5f, 0.6f});
+					rating->setZOrder(event->getSprite()->getZOrder() - 1);
+					// `event->getSprite()` is guaranteed to not be `nullptr` though
+					event->getSprite()->addChildAtPosition(rating, Anchor::Center);
+				}
+			}
+		}
         // You should always propagate Geode UI events
         return ListenerResult::Propagate;
     });
+	new EventListener<EventFilter<ModPopupUIEvent>>(+[](ModPopupUIEvent* event) {
+		if (event->getPopup())
+	})
 }
