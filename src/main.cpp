4df643904886085df76137a82eecdc20a6ec7105
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-07-30 23:25:02 -0400

    fix json

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <Geode/Geode.hpp>
 #include <Geode/ui/GeodeUI.hpp>
+#include <Geode/utils/web.hpp>
+#include <Geode/loader/Event.hpp>
 
 using namespace geode::prelude;
 
+EventListener<web::WebTask> m_webListener;
+
 $execute {
 	new EventListener<EventFilter<ModLogoUIEvent>>(+[](ModLogoUIEvent* event) {
+		m_webListener.bind([] (web::WebTask::Event* e) {
+			if (web::WebResponse* res = e->getValue()) {
+                if (res->ok()) {
+					auto json = res->json();
+				}
+            } else if (e->isCancelled()) {
+                log::info("The request was cancelled... So sad :(");
+            }
+		});
+
+		auto req = web::WebRequest();
+		m_webListener.setFilter(req.get("https://raw.githubusercontent.com/LimeGradient/modratings/master/ratings.json"));
+
         if (event->getModID() == "geode.loader") {
             // Remember: no assumptions, even trivial ones!
             if (auto rating = CCSprite::createWithSpriteFrameName("GJ_epicCoin3_001.png")) {
